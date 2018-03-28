ab7138d53b5997b37157faa23fbd9f7fd133eba7
tiagochst <tiagochst@gmail.com>
2018-03-28 17:04:03 -0300

    update

--- R/global.R
+++ R/global.R
@@ -7,7 +7,7 @@
 #' }
 #' TCGAbiolinksGUI(run = FALSE)
 #' @name TCGAbiolinksGUI
-#' @import shiny shinyFiles shinydashboard downloader caret
+#' @import shiny shinyFiles shinydashboard downloader
 #' TCGAbiolinks ggplot2 shinyBS stringr ggrepel pathview ELMER grid
 #' clusterProfiler parallel readr data.table maftools
 #' @importFrom SummarizedExperiment SummarizedExperiment values rowRanges colData<- assay colData
